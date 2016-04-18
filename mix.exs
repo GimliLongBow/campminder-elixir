e17f72ac686ee74aebcd3eb9036bf7986e3a0843
Andrew Anderson <gimlilongbow@gmail.com>
2016-04-18 09:35:33 -0400

    Multiple Elixir versions.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CampMinder.Mixfile do
      version: "0.1.0",
      description: "A CampMinder API library for Elixir.",
      package: package,
-     elixir: "~> 1.2",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(Mix.env)]
